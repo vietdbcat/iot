823f1debd0014fef8ea41ce7672044dadd49c6c9
VietDB <vietcr999@gmail.com>
2023-12-23 00:05:57 +0700

    12:05

---
+++ templates/person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, ten, msv, ngaysinh, diachi, lop):
+        self.ten = ten
+        self.msv = msv
+        self.ngaysinh = ngaysinh
+        self.lop = lop
+        self.diachi = diachi
